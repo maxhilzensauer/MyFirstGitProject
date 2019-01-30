23d51157e0c2bedeea3727bc090838225e63e742
maxhilzensauer <max.hilzensauer@htl-salzburg.ac.at>
2019-01-30 17:27:47 +0100

    initial commit

---
+++ Assets/Scripts/TestObject.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestObject : MonoBehaviour {
+
+    string HelloWorld = "Hello World";
+
+    public void PrintHelloWorld()
+    {
+        Debug.Log(HelloWorld);
+    }
+
+
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
